288e35a7356bc08521737f90952b2e1a7b63d3e6
jasmeet17 <jasmeet17mast@gmail.com>
2018-06-08 17:03:42 -0700

    call fortune method

--- src/com/spring/demo/HelloSpringApp.java
+++ src/com/spring/demo/HelloSpringApp.java
@@ -12,10 +12,14 @@ public class HelloSpringApp {
 		//retrieve bean from Spring container
 		Coach theCoach = context.getBean("myCoach", Coach.class);
 		Coach criCoach = context.getBean("myCricketCoach", Coach.class);
+		Coach baseBallCoach = context.getBean("myBaseballCoach", Coach.class);
 		
 		//call methods on the bean
 		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
 		System.out.println(criCoach.getDailyWorkout());
+		System.out.println(baseBallCoach.getDailyWorkout());
+		System.out.println(baseBallCoach.getDailyFortune());
 		
 		//close the context
 		context.close();
