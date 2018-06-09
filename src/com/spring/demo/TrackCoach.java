5f1946af7c4756aee60dd61cefd291b47e5bc39c
jasmeet17 <jasmeet17mast@gmail.com>
2018-06-09 15:32:05 -0700

    Custom hooks in Bean Lifecycle - init and destroy methods

--- src/com/spring/demo/TrackCoach.java
+++ src/com/spring/demo/TrackCoach.java
@@ -18,5 +18,15 @@ public class TrackCoach implements Coach {
 	public String getDailyFortune() {
 		return "Just do it:" + this.fortuneService.getFortune();
 	}
+	
+	// add an init method
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach Inside method: doMyStartupStuff");
+	}
+	
+	// add a destroy method
+	public void doMyCleanUpStuff() {
+		System.out.println("TrackCoach Inside method: doMyCleanUpStuff");
+	}
 
 }
